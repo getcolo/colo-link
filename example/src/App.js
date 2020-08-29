fec5e7bba0bc24fed3bb3926a58905a2288a3787
Shreyas Jaganmohan <drshrey@Shreyass-MacBook-Pro.local>
2020-08-29 09:43:45 -0700

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'colo-link'
+import 'colo-link/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
